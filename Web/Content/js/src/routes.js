5cc33a5f330280515cbf30d50492e8395006974a
Marcin <walas.marcin@gmail.com>
2015-12-22 10:49:28 +0100

    initial setup of react router

---
+++ Web/Content/js/src/routes.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+import React from 'react';
+import { Router, Route, DefaultRoute, Link } from 'react-router';
+
+import App from './app.jsx';
+import HomePage from './components/homePage.jsx';
+import AuthorsPage from './components/authorsPage.jsx';
+
+let routes = (
+    <Route name="app" path="/" handler={App}>
+        <DefaultRoute handler={HomePage} />
+        <Route name="authors" handler={AuthorsPage} />
+        <Route name="about" handler={AboutPage} />
+    </Route>
+);
+
+export default routes;
