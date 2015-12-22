7db80e19f4047bc7d21b8069985480b0235dee43
Marcin <walas.marcin@gmail.com>
2015-12-22 12:39:02 +0100

    added react router

---
+++ Web/Content/js/src/main.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import Routes from './routes';
+import createHistory from 'history/lib/createHashHistory';
+
+
+ReactDOM.render(<Router>{Routes}</Router>, document.getElementById('app'));
