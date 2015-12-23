59043c9cc058c4a399cd99c4ca73d95b77fbe1ec
Marcin <walas.marcin@gmail.com>
2015-12-23 10:22:31 +0100

    added 404 error page

---
+++ Web/Content/js/src/components/NotFoundPage.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+var NotFoundPage = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Page not found</h1>
+                <Link to={`/`}>Back to Home</Link>
+            </div>
+        );
+    }
+});
+
+export default NotFoundPage;
--- Web/Content/js/src/routes.js
+++ Web/Content/js/src/routes.js
@@ -7,12 +7,14 @@ import App from './components/app';
 import HomePage from './components/homePage';
 import AuthorsPage from './components/authorsPage';
 import AboutPage from './components/aboutPage';
+import NotFoundPage from './components/notFoundPage';
 
 let routes = (
     <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
+        <IndexRoute component={HomePage} /> 
         <Route path="authors" component={AuthorsPage} />
         <Route path="about" component={AboutPage} />
+        <Route path="*" component={NotFoundPage}/>
     </Route>
 );
 
